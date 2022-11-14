f03dad55602f62ad34140a6ca1f4ffde9c0983d4
NetanelMaman <netanel.ma@gmail.com>
2022-11-14 11:16:10 +0200

    first commit

--- src/Utils/AppConfig.tsx
+++ src/Utils/AppConfig.tsx
@@ -1,14 +1,19 @@
 
 class AppConfig {
     // general configuration
+    public productsUrl =""
 }
 
 class DevConfig extends AppConfig {
     // dev configuration
+    public productsUrl ="https://localhost:3030/api/products";
+
 }
 
 class ProdConfig extends AppConfig {
     // Prod configuration
+    public productsUrl ="https://localhost:3030/api/products";
+
 }
 
 const appConfig = process.env.NODE_ENV === "development" ? new DevConfig() : new ProdConfig();
