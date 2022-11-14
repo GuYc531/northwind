74a3ad3f878eb36edea09d4eb36299550e2c09a3
NetanelMaman <netanel.ma@gmail.com>
2022-11-14 13:32:13 +0200

    update

---
+++ src/Models/ProductModel.ts
@@ -0,0 +1,11 @@
+
+class ProductModel {
+	public id: number;
+    public name: string;
+    public price: number;
+    public stock: number;
+    public imageName: string;
+
+}
+
+export default ProductModel;
--- src/Utils/AppConfig.tsx
+++ src/Utils/AppConfig.tsx
@@ -1,18 +1,22 @@
 
 class AppConfig {
     // general configuration
-    public productsUrl =""
+    public productsUrl = "";
+    public productsImageUrl = "";
 }
 
 class DevConfig extends AppConfig {
     // dev configuration
-    public productsUrl ="https://localhost:3030/api/products";
+    public productsUrl = "http://localhost:3030/api/products/";
+    public productsImageUrl = "http://localhost:3030/api/products/images/";
 
 }
 
 class ProdConfig extends AppConfig {
     // Prod configuration
-    public productsUrl ="https://localhost:3030/api/products";
+    public productsUrl = "https://www.northwind/api/products";
+    public productsImageUrl = "http://www.northwind/api/products/images/";
+
 
 }
 
